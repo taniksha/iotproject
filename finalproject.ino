cce6708c228f86aaa52c7bb69cb5a991eddbaf58
taniksha <sharma.taniksha03@gmail.com>
2017-06-29 16:47:46 +0530

    Add files via upload

---
+++ finalproject.ino
@@ -0,0 +1,101 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <IRremoteESP8266.h>
+
+int RECV_PIN = 2; //an IR detector/demodulatord is connected to GPIO pin D2
+// Update these with values suitable for your network.
+const char* ssid = "tanu";
+const char* password = "7742609532";
+const char* mqtt_server = "broker.mqttdashboard.com";
+//const char* mqtt_server = "iot.eclipse.org";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup_wifi() {
+   delay(100);
+  // We start by connecting to a WiFi network
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+      delay(500);
+      Serial.print(".");
+    }
+  randomSeed(micros());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) 
+{
+  
+} //end callback
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) 
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    //if you MQTT broker has clientID,username and password
+    //please change following line to    if (client.connect(clientId,userName,passWord))
+    if (client.connect(clientId.c_str()))
+    {
+      Serial.println("connected");
+     //once connected to MQTT broker, subscribe command if any
+      client.subscribe("OsoyooCommand");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+} //end reconnect()
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  long now = millis();
+  
+  if (now - lastMsg > 2000) {
+     lastMsg = now;
+    if (irrecv.decode(&results)) 
+    {
+      Serial.println(results.value, HEX);
+      irrecv.resume(); // Receive the next value
+      String msg="Receive value: ";
+       msg= msg+ results.value;
+       char message[58];
+       msg.toCharArray(message,58);
+       Serial.println(message);
+       //publish sensor data to MQTT broker
+      client.publish("OsoyooData", message);
+     }
+  }
+}
+
